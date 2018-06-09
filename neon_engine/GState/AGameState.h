6fcbfc7ff35a8701a237b7b8da2535b810520b94
Theo Penavaire <theo.penavaire@gmail.com>
2018-06-09 17:52:15 +0200

    Add abstraction for Game State (Neon Engine)

---
+++ neon_engine/GState/AGameState.h
@@ -0,0 +1,25 @@
+//  Created by Theo Penavaire on 06/09/2018
+//  Last Update on 06/09/2018 
+
+#include "IGameState.h"
+#include "IGObject.h"
+
+namespace neon_engine
+{
+	
+	class AGameState : public IGameState
+	{
+		
+	public:
+		virtual ~AGameState() = default;
+
+	protected:
+		Sptr<Core>		_ge;
+
+		std::vector<Sptr<IGObject>>	_gameObjects;
+		NEventFuncMapper			_eventFuncMapper;
+
+		virtual void setEvenFuncMapper() = 0;
+	};
+
+}
